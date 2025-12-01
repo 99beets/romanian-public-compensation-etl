a6a834b29d3e77a7e793808fb4b59524befe852e
Paul Berciu <berciu.paulstefan@gmail.com>
2025-12-01 14:35:50 +0000

    Remove credentials from env script

--- tools/set_pg_env.sh
+++ tools/set_pg_env.sh
@@ -1,7 +1,7 @@
 export PG_HOST="localhost"
 export PG_PORT="5432"
 export PG_DB="indemnizatii"
-***REMOVED***
-***REMOVED***
+export PG_USER="${PGUSER}"
+export PG_PASSWORD="${PGPASSWORD}"
 
 echo "PostgreSQL environment variables set."
