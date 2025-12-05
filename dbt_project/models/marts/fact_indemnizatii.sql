3d16fc524bdd7605ba52abcf3e365d22e04713a8
Paul Berciu <berciu.paulstefan@gmail.com>
2025-12-05 17:48:50 +0000

    Introduce annual fact model for remuneration dataset
    - Fact table joins normalized entitties and enriched values
    - Uses dbt variable for reporting year
    - Represents one record per entity/person/year

---
+++ dbt_project/models/marts/fact_indemnizatii.sql
@@ -0,0 +1,29 @@
+WITH b AS (
+    SELECT *
+    FROM {{ ref('int_indemnizatii_base') }}
+),
+
+p AS (
+    SELECT *
+    FROM {{ ref('int_persoane_clean') }}
+),
+
+c AS (
+    SELECT *
+    FROM {{ ref('int_companii_clean') }}
+)
+
+SELECT
+    b.pk,
+    p.person_id,
+    c.company_id,
+    b.total_plata,
+    b.suma_clean,
+    b.variabila_clean,
+
+    {{ var('indemnizatii_var') }} AS an_raportare
+FROM b
+LEFT JOIN p
+    ON trim(upper(p.nume_normalizat)) = trim(upper(b.nume))
+LEFT JOIN c
+    ON c.company_id = b.cui
