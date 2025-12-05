a5224d179feb70606b02ebb5e8fb9b80c535aef3
Paul Berciu <berciu.paulstefan@gmail.com>
2025-12-05 18:28:40 +0000

    chore: formatting

--- dbt_project/models/staging/stg_indemnizatii_clean.sql
+++ dbt_project/models/staging/stg_indemnizatii_clean.sql
@@ -1,38 +1,29 @@
-with source as (
+WITH src AS (
 
-    select *
-    from {{ source('indemnizatii_source', 'indemnizatii_clean') }}
+    SELECT *
+    FROM {{ source('indemnizatii_source', 'indemnizatii_clean') }}
 
 ),
 
-cleaned as (
+cleaned AS (
 
-    select
-        -- Use actual physical primary key
-        id as pk,
-
+    SELECT
+        id AS pk,
         nr_crt,
         autoritate_tutelara,
         intreprindere,
         cui,
-
-        -- Rename ambiguous columns
-        personal as nume,
-        calitate_membru as functie,
-
-        -- Keep raw text values
-        suma as suma_raw,
-        indemnizatie_variabila as variabila_raw,
-
-        -- Clean numeric values provided by ETL
-        suma_num as suma_clean,
-        indemnizatie_variabila_num as variabila_clean,
-
+        personal AS nume,
+        calitate_membru AS functie,
+        suma AS suma_raw,
+        indemnizatie_variabila AS variabila_raw,
+        suma_num AS suma_clean,
+        indemnizatie_variabila_num AS variabila_clean,
         created_at
 
-    from source
+    FROM src
 
 )
 
-select *
-from cleaned
+SELECT *
+FROM cleaned
