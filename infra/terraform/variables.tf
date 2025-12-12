42e50d56066d0995f4766604eab43d1311d33bec
Paul Berciu <berciu.paulstefan@gmail.com>
2025-12-12 13:10:30 +0000

    Add database variables and expand project tag structure

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -14,9 +14,31 @@ variable "tags" {
   description = "Common resource tag"
   type        = map(string)
   default = {
-    project = "romanian-public-comp-etl"
-    owner   = "data-engineer"
-    env     = "dev"
+    project     = "romanian-public-comp-etl"
+    owner       = "data-engineer"
+    env         = "dev"
     cost_center = "learning"
   }
+}
+
+variable "db_name" {
+  description = "PostgreSQL database name"
+  type        = string
+  default     = "romanian_comp"
+}
+
+variable "db_username" {
+  description = "Master username for RDS Postgres"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Master password for RDS Postgres"
+  type        = string
+}
+
+variable "db_allowed_cidrs" {
+  description = "CIDR blocks allowed to connect to RDS"
+  type        = list(string)
+  default     = ["82.0.130.203/32"]
 }
